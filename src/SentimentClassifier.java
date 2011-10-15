c70484bf784dc73c8df527dee8107f161f5d57d0
ADTC <send2adtc@gmail.com>
2011-10-15 10:12:19 +0000

    Adding src folder and SentimentClassifier

---
+++ src/SentimentClassifier.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lewis
+ *
+ */
+public class SentimentClassifier {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
